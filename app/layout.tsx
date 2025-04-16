42fa3afccd2d91c8481e1f818fb6b5534740db54
Myrat Charyyev <charyyev2110@gmail.com>
2025-04-15 20:29:06 -0500

    setting up analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,11 +44,10 @@ export default function RootLayout({
               c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
               t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
               y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "r4oaogc0vz");`
+            })(window, document, "clarity", "script", '${process.env.CLARITY_PROJECT_ID}');`
           }}
         />
       </head>
-
       {/* <iframe
           width="540"
           height="305"
